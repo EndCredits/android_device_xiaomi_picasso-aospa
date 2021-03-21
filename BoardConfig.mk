8de4b3d6185cb4ea1e94cfec0f90503f654aaca7
Alexander Koskovich <zvnexus@outlook.com>
2021-03-20 18:50:03 -0700

    picasso: Import HIDL manifest from LA.UM.9.12.r1-13500.01-SMxx50.QSSI12.0
     * With some slight differences, and formatted.
       QTI Radio | 2.4 -> 2.6
       Radio IMS | 1.6 -> 1.7
       IMS Factory | 1.0 > 1.1
       QTI Factory | 2.2 > 2.3
       Radio LPA | 1.0 -> 1.1
       UCEService | 2.2 -> 2.3
    
    * Kill authsecret and strongbox
    
     * Point HIDL matrix to QCOM's compatiblity matrix.
    
    Change-Id: I399fb84d3951f201a2b151401f5cd235c467c452
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,6 +52,13 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 # Display
 TARGET_SCREEN_DENSITY := 440
 
+# HIDL
+DEVICE_MANIFEST_FILE += \
+    $(DEVICE_PATH)/configs/vintf/manifest.xml
+
+DEVICE_MATRIX_FILE += \
+    device/qcom/common/compatibility_matrix.xml
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 cgroup.memory=nokmem,nosocket loop.max_part=7 reboot=panic_warm
 
