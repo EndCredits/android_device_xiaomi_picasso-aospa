ad08d4d868bcfd41d9041540480f2398b6c63263
EndCredits <alicization.han@gmail.com>
2022-07-26 23:43:43 +0800

    picasso: Set init_fatal_reboot_target to recovery
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,6 +73,7 @@ BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 cgroup.memory=nokmem,nosocket loop.max_part=7 reboot=panic_warm
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_KERNEL_IMAGE_NAME := Image
