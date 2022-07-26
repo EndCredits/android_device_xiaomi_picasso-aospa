85732035522dfbcb593ab8d795a0281ebcc8e97c
EndCredits <alicization.han@gmail.com>
2022-07-26 23:43:15 +0800

    picasso: Define kernel image name
    Signed-off-by: EndCredits <alicization.han@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,6 +75,7 @@ BUILD_BROKEN_ENFORCE_SYSPROP_OWNER := true
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 cgroup.memory=nokmem,nosocket loop.max_part=7 reboot=panic_warm
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
+BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 
